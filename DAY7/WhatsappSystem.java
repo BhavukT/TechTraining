3b477f17e1c8000ac281c7450f7a40459971146a
BhavukT <bhavuk.tomar@bigohtech.com>
2022-01-27 11:32:24 +0530

    first commit 27th JAN

--- DAY7/WhatsappSystem.java
+++ DAY7/WhatsappSystem.java
@@ -5,16 +5,71 @@
 // User can post status
 // Status can be text and image
 
-class WhatsappSystem {
-    public void postStatus(String type, String message){
-        this.type = type;
-        this.message = message;
+class Contacts extends WhatsappSystem {
+    long pNumber;
+
+    Contacts(long pNumber) {
+        this.pNumber = pNumber;
     }
-     public void sendMessage(String type, String message, double pNumber){
-        System.out.println("Message Sent 🔃");
-     }
+
+    bool isContactAvailable(long fNumber, long pNumber){
+        return true;
+    }
+}
+
+class Message extends WhatsappSystem {
+    public String type = " ";
+
+    Message(){
+
+    }
+
+    public void sendMessage(String type, String message, double pNumber){
+        if(type == "Text"){
+            System.out.println("Text Message - " + message);
+        }
+        else if(type == "MP3"){
+            system.out.println("MP3 Message - " + message);
+        }
+        else if(type == "MP4"){
+            System.out.println("MP4 Message - " + message);
+        }
+    }    
+}
+
+class Status extends WhatsappSystem{
+    public String type = " ";
+
+    Status(){
+
+    }
+
+    public void postStatus(String type, String message){
+        if(type == "Text"){
+            System.out.println("Text Status - " + message);
+        }
+        else if(type == "MP3"){
+            system.out.println("MP3 Status - " + message);
+        }
+        else if(type == "MP4"){
+            System.out.println("MP4 Status - " + message);
+        }
+    }  
+}
+
+class WhatsappSystem {
+    WhatsappSystem(){
+        
+    }
 }
 
 class User extends WhatsappSystem {
-    
+    double pNumber;
+
+    public double getNumber(){
+        return pNumber;
+    }
+    User(double pNumber){
+        this.pNumber = pNumber;
+    }
 }
